4ff9e5459ad837238f96f0e90f453fcd567a42cd
Rafael Cavalcanti <rccavalcanti@gmail.com>
2017-05-25 07:52:37 -0300

    Unify Ruby files identation to 2 spaces

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ end
 # Check for DB playlist
 pl = mpd.playlists.find { |p| p.name == "dbpl" }
 if pl.nil?
-    update_db(mpd)
+  update_db(mpd)
 end
 
 # Load JSONs
@@ -41,48 +41,48 @@ streams_private = load_streams("streams_private.json")
 
 # Routes
 get '/' do
-	  hostname = `uname -n`.chop.capitalize
-    erb :main, locals: { hostname: hostname, streams: streams }
+	hostname = `uname -n`.chop.capitalize
+  erb :main, locals: { hostname: hostname, streams: streams }
 end
 
 get '/s' do
-	  hostname = `uname -n`.chop.capitalize
-    erb :main, locals: { hostname: hostname, streams:
-			streams_private.merge({"Rio de Janeiro":""}).merge(streams) }
+	hostname = `uname -n`.chop.capitalize
+  erb :main, locals: { hostname: hostname, streams:
+		streams_private.merge({"Rio de Janeiro":""}).merge(streams) }
 end
 
 get '/api/:cmd' do
-    cmd = params[:cmd]
-    case cmd
-    when "play"
-		    mpd.play
-    when "stop"
-		    mpd.stop
+  cmd = params[:cmd]
+  case cmd
+  	when "play"
+	  	mpd.play
+  	when "stop"
+		  mpd.stop
     when "vdown"
-		    mpd.send_command("volume -5")
+		  mpd.send_command("volume -5")
     when "vup"
-        mpd.send_command("volume +5")
+      mpd.send_command("volume +5")
     when "vol" # tried as return value for vdown/vup, but seemed slower
-        mpd.volume.to_s + '%'
+      mpd.volume.to_s + '%'
     when "state"
-        { :playing => mpd.playing?,
-					:name => get_name(mpd, streams.merge(streams_private)),
-					:playing_local => playing_local?(mpd) }.to_json
+      { :playing => mpd.playing?,
+				:name => get_name(mpd, streams.merge(streams_private)),
+				:playing_local => playing_local?(mpd) }.to_json
     when "play-url"
-        play_url(params, mpd)
+      play_url(params, mpd)
     when "play-random"
-        play_random(mpd)
-    end
+      play_random(mpd)
+  end
 end
 
 get '/update' do
-	  streams = load_streams("streams.json")
-	  streams_private = load_streams("streams_private.json")
-    update_db(mpd)
-    "<a href=\"/\">Listas de streams, DB e playlist DB atualizados.</a>"
+	streams = load_streams("streams.json")
+	streams_private = load_streams("streams_private.json")
+  update_db(mpd)
+  "<a href=\"/\">Listas de streams, DB e playlist DB atualizados.</a>"
 end
 
 error do
-    '<h3>Desculpe, ocorreu um erro.</h3><p>Mensagem: ' + \
-        env['sinatra.error'].message + '</p><a href="/"><h2>Voltar</h2></a>'
+  '<h3>Desculpe, ocorreu um erro.</h3><p>Mensagem: ' + \
+    env['sinatra.error'].message + '</p><a href="/"><h2>Voltar</h2></a>'
 end
--- methods.rb
+++ methods.rb
@@ -1,12 +1,12 @@
 def get_name(mpd, streams)
-    song = mpd.current_song
-    if song.nil?                               # Se nada
-        ""
-    elsif ! song.file.include?("://")          # Se é local
-        get_local_name(song)
-    else                                       # Se é remota
-        streams.key(song.file) || song.file
-    end
+  song = mpd.current_song
+  if song.nil?                               # Se nada
+    ""
+  elsif ! song.file.include?("://")          # Se é local
+    get_local_name(song)
+  else                                       # Se é remota
+    streams.key(song.file) || song.file
+  end
 end
 
 def get_local_name(song)
@@ -20,23 +20,23 @@ def get_local_name(song)
 end
 
 def playing_local?(mpd)
-    mpd.playing? && ! mpd.current_song.file.include?("://")
+  mpd.playing? && ! mpd.current_song.file.include?("://")
 end
 
 def update_db(mpd)
-    # Update DB
-    mpd.update
+  # Update DB
+  mpd.update
 
-    # Update/create playlist with whole DB
+  # Update/create playlist with whole DB
+  pl = mpd.playlists.find { |p| p.name == "dbpl" }
+  if pl.nil?
+    mpd.save "dbpl"
     pl = mpd.playlists.find { |p| p.name == "dbpl" }
-    if pl.nil?
-        mpd.save "dbpl"
-        pl = mpd.playlists.find { |p| p.name == "dbpl" }
-    end
-    pl.clear
-    for song in mpd.songs
-        pl.add song
-    end
+  end
+  pl.clear
+  for song in mpd.songs
+    pl.add song
+  end
 end
 
 # Load JSON ignoring keys starting with //
@@ -50,24 +50,24 @@ def load_streams(file_name)
 end
 
 def play_url(params, mpd)
-    url = params[:url].strip
-    mpd.clear
-    mpd.add url
-    mpd.play
+  url = params[:url].strip
+  mpd.clear
+  mpd.add url
+  mpd.play
 end
 
 def play_random(mpd)
-    # Se está tocando arquivo local, tocar o próximo
-    if playing_local?(mpd)
-        mpd.next
-    # Se não está tocando arquivo local, carregar toda a
-  	# biblioteca e começa a tocar em modo aleatório
-    else
-        mpd.clear
-        pl = mpd.playlists.find { |p| p.name == "dbpl" }
-        pl.load
-        mpd.random= true
-		    mpd.crossfade= 5
-        mpd.play
-    end
+  # Se está tocando arquivo local, tocar o próximo
+  if playing_local?(mpd)
+    mpd.next
+  # Se não está tocando arquivo local, carregar toda a
+  # biblioteca e começa a tocar em modo aleatório
+  else
+    mpd.clear
+    pl = mpd.playlists.find { |p| p.name == "dbpl" }
+    pl.load
+    mpd.random= true
+	  mpd.crossfade= 5
+    mpd.play
+  end
 end
