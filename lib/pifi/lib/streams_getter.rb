8bb82057f7416e1eaebb3e81198e40f81a7b4d56
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-22 14:11:40 -0300

    StreamsGetter: do path checking and raise friendly msg

--- lib/pifi/lib/streams_getter.rb
+++ lib/pifi/lib/streams_getter.rb
@@ -4,9 +4,10 @@ module PiFi
   class StreamsGetter
     include Utils
 
-    def initialize(path_pub, path_priv)
+    def initialize(path_pub, path_priv="")
       @path_pub = path_pub
       @path_priv = path_priv
+      check_paths
     end
 
     def streams
@@ -22,6 +23,14 @@ module PiFi
       all = pub.merge(priv)
 
       {pub: pub, all: all}
+    end
+
+    def check_paths
+      raise ArgumentError, "Streams file not found at '#{@path_pub}'" unless File.file?(@path_pub)
+      if ! @path_priv.empty? && ! File.file?(@path_priv)
+        warn "Additional streams file not found at '#{@path_priv}'"
+        @path_priv = ""
+      end
     end
   end
 end
