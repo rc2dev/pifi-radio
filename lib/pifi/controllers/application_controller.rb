ea6ac7a8f498e724fd39a8884286879984098922
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-25 17:48:33 -0300

    backend: Add header for CORS in development mode

--- lib/pifi/controllers/application_controller.rb
+++ lib/pifi/controllers/application_controller.rb
@@ -6,11 +6,13 @@ module PiFi
   class ApplicationController < Sinatra::Base
     set ConfigGetter.new.config
     set :streams, StreamsGetter.new(settings.streams_path, settings.streams_path_priv).streams
-
     set :root, File.expand_path("../../", __FILE__)
 
     configure :production do
       set :static, settings.serve_static
+    end
+    configure :development do
+      before { response.headers["Access-Control-Allow-Origin"] = "*" }
     end
   end
 end
