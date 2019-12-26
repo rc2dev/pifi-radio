1ff49e90042f3657332e8eaf0a8eb8e29e4b2545
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-26 14:53:31 -0300

    backend: Remove language support

--- lib/pifi/controllers/index_controller.rb
+++ lib/pifi/controllers/index_controller.rb
@@ -1,15 +1,10 @@
 require "pifi/controllers/application_controller"
-require "pifi/lib/lang_chooser"
 
 module PiFi
   class IndexController < ApplicationController
     def title
       title = "PiFi Radio"
       settings.production? ? title : "[#{settings.environment.capitalize}] #{title}"
-    end
-
-    def lang
-      LangChooser.new(env["HTTP_ACCEPT_LANGUAGE"]).lang
     end
 
     def play_local?
