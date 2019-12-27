0a32487178477bc11d0da0d65c7f29f57bd90948
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-27 05:35:55 -0300

    frontend: CSS fixes/tweaks

--- frontend/src/components/backdrop.jsx
+++ frontend/src/components/backdrop.jsx
@@ -3,8 +3,8 @@ import './backdrop.scss';
 
 const Backdrop = ({ title, body }) => {
   return (
-    <div className={title ? 'backdrop backdrop--visible' : 'backdrop'}>
-      <h3 className="ellipsis">{title}</h3>
+    <div className={title ? 'p-2 backdrop backdrop--visible' : 'p-2 backdrop'}>
+      <h3>{title}</h3>
       <h5>{body}</h5>
     </div>
   );
