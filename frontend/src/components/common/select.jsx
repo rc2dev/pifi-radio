0f4705e6423c97b2af52afb11bbb2da08d8f6110
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-29 00:02:18 -0300

    frontend: Extract Select as component

---
+++ frontend/src/components/common/select.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Select = ({ id, label, row, data, ...rest }) => (
+  <div className={row ? "form-group row" : "form-group"}>
+    <label htmlFor={id} className="col-sm-2 col-form-label">
+      {label}
+    </label>
+    <div className="col-sm-10">
+      <select id={id} className="form-control" {...rest}>
+        {data.map(d => (
+          <option key={d}>{d}</option>
+        ))}
+      </select>
+    </div>
+  </div>
+);
+
+export default Select;
--- frontend/src/components/settings.jsx
+++ frontend/src/components/settings.jsx
@@ -4,62 +4,37 @@ import { getTheme, changeTheme, themes } from '../theme';
 import i18next from 'i18next';
 import { withTranslation } from 'react-i18next';
 import { languages } from '../config.json';
+import Select from './common/select';
 
 const Settings = ({ t }) => {
   const renderFooter = () => (
     <button className="btn btn-secondary" data-dismiss="modal">
-      Close
+      {t('close')}
     </button>
-  );
-
-  const renderLanguage = () => (
-    <div className="form-group row">
-      <label htmlFor="language-selector" className="col-sm-2 col-form-label">
-        {`${t('language')}:`}
-      </label>
-      <div className="col-sm-10">
-        <select
-          id="language-selector"
-          className="form-control"
-          value={i18next.language}
-          onChange={e => {
-            i18next.changeLanguage(e.target.value);
-          }}
-        >
-          {languages.map(lang => (
-            <option key={lang}>{lang}</option>
-          ))}
-        </select>
-      </div>
-    </div>
-  );
-
-  const renderTheme = () => (
-    <div className="form-group row">
-      <label htmlFor="theme-selector" className="col-sm-2 col-form-label">
-        {`${t('theme')}:`}
-      </label>
-      <div className="col-sm-10">
-        <select
-          id="theme-selector"
-          className="form-control"
-          value={getTheme()}
-          onChange={e => {
-            changeTheme(e.target.value);
-          }}
-        >
-          {themes.map(theme => (
-            <option key={theme}>{theme}</option>
-          ))}
-        </select>
-      </div>
-    </div>
   );
 
   return (
     <Modal id="settings" title={t('settings')} footer={renderFooter()}>
-      {renderLanguage()}
-      {renderTheme()}
+      <Select
+        id="theme-select"
+        label={t('theme')}
+        data={themes}
+        row
+        value={getTheme()}
+        onChange={e => {
+          changeTheme(e.target.value);
+        }}
+      />
+      <Select
+        id="language-select"
+        label={t('language')}
+        data={languages}
+        row
+        value={i18next.language}
+        onChange={e => {
+          i18next.changeLanguage(e.target.value);
+        }}
+      />
     </Modal>
   );
 };
