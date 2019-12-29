ee32db91c0fe80e6a314724ef06dba49e8bb59fa
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-29 05:19:33 -0300

    frontend: Add logo to navbar

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 import './navBar.scss';
+import logo from '../logo.svg';
 
 const NavBar = ({ t }) => (
-  <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+  <nav className="navbar navbar-expand-sm navbar-dark bg-primary ">
     <a className="navbar-brand" href="/">
+      <img
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-text-bottom mr-2"
+        alt=""
+      />
       PiFi Radio
     </a>
     <button
@@ -19,7 +27,7 @@ const NavBar = ({ t }) => (
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
+      <div className="navbar-nav pt-1 ml-2">
         {/* We use buttons and different class names instead of Bootstrap's
            anchor, so we don't get a warning for href="#". */}
         <button
