bffe93a81cb7640a23911c028b1bda687e24a1e9
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-29 07:55:16 -0300

    backend: Clean-up

--- lib/pifi/controllers/index_controller.rb
+++ lib/pifi/controllers/index_controller.rb
@@ -2,15 +2,6 @@ require "pifi/controllers/application_controller"
 
 module PiFi
   class IndexController < ApplicationController
-    def title
-      title = "PiFi Radio"
-      settings.production? ? title : "[#{settings.environment.capitalize}] #{title}"
-    end
-
-    def play_local?
-      settings.play_local
-    end
-
     get "/" do
       send_file File.join(settings.public_folder, 'index.html')
     end
