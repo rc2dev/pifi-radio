e1b8d6e5c9f0617a1ca965049ca5fd4a58a0afad
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-29 01:47:26 -0300

    frontend: Remove scrolling from backdrop

--- frontend/src/components/backdrop.jsx
+++ frontend/src/components/backdrop.jsx
@@ -3,10 +3,21 @@ import './backdrop.scss';
 
 const Backdrop = ({ title, body }) => {
   let classes = 'backdrop p-2 text-white';
-  if (title) classes += ' backdrop--visible';
 
+  const getScrollbarWidth = () =>
+    window.innerWidth - document.documentElement.clientWidth;
+
+  if (title) {
+    classes += ' backdrop--visible';
+    document.body.style.paddingRight = getScrollbarWidth() + 'px';
+    document.body.classList.add('body--backdrop');
+  } else {
+    document.body.classList.remove('body--backdrop');
+    document.body.style.paddingRight = 0;
+  }
+
+  // We need text-white on the h* because some themes override it
   return (
-    // We need text-white on the h* because some themes override it
     <div className={classes}>
       <h3 className="text-white">{title}</h3>
       <h5 className="text-white">{body}</h5>
