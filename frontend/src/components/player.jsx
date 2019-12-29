8e984f877eef8a1719810803b69d9f2ffa5fcb17
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-12-29 01:30:40 -0300

    frontend: Fix player scrolling (set top dinamically)

--- frontend/src/components/player.jsx
+++ frontend/src/components/player.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PlayerStatus from './playerStatus';
 import PlayerControls from './playerControls';
 import './player.scss';
 
-const Player = ({ playerStatus }) => (
-  <div className="player">
-    <PlayerStatus playerStatus={playerStatus} />
-    <PlayerControls playerStatus={playerStatus} />
-  </div>
-);
+const Player = ({ playerStatus }) => {
+  useEffect(positionPlayerTop);
+
+  return (
+    <div className="player">
+      <PlayerStatus playerStatus={playerStatus} />
+      <PlayerControls playerStatus={playerStatus} />
+    </div>
+  );
+};
+
+// Calculate and set top for Player
+// Needed for desktop view with sticky
+const positionPlayerTop = () => {
+  const navHeight = document.querySelector('nav').clientHeight;
+  const topMargin = 24;
+  document.querySelector('.player').style.top = navHeight + topMargin + 'px';
+};
+
 export default Player;
