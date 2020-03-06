e963357d0b2ae3d9d80f4b05d48223da45e2029d
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-06 12:40:39 -0300

    Improve frontend-backend connection on dev mode

--- lib/pifi/controllers/application_controller.rb
+++ lib/pifi/controllers/application_controller.rb
@@ -5,10 +5,6 @@ require "sinatra/base"
 module PiFi
   class ApplicationController < Sinatra::Base
     set :static, false
-    configure :development do
-      before { response.headers["Access-Control-Allow-Origin"] = "*" }
-    end
-
     set ConfigGetter.new.config
     set :streams, Streams.new(settings.streams_path, settings.streams_path_priv)
   end
