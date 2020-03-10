f7c6d9b80c2a7055e49feb666a4eca0b721a9376
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-10 10:49:14 -0300

    Some refactor

--- frontend/src/components/common/backdrop.jsx
+++ frontend/src/components/common/backdrop.jsx
@@ -3,20 +3,21 @@ import { getScrollbarWidth } from '../../utils/style';
 import './backdrop.scss';
 
 const Backdrop = ({ title, body }) => {
+  const visible = title;
   const bodyStyled = document.body.classList.contains('body--backdrop');
 
-  if (title && !bodyStyled) {
+  if (visible && !bodyStyled) {
     // Calculate this BEFORE adding the class.
     document.body.style.paddingRight = getScrollbarWidth() + 'px';
     document.body.classList.add('body--backdrop');
     // We need this check because Bootstrap modals also style paddingRight
-  } else if (!title && bodyStyled) {
+  } else if (!visible && bodyStyled) {
     document.body.style.paddingRight = 0;
     document.body.classList.remove('body--backdrop');
   }
 
   const classes =
-    'backdrop p-2 text-white' + (title ? ' backdrop--visible' : '');
+    'backdrop p-2 text-white' + (visible ? ' backdrop--visible' : '');
 
   // We need text-white on h* tags because some themes override it
   return (
