fa3b214728e684007dd9007ef4cfdc2af3414fb1
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-03-12 13:26:47 -0300

    frontend: Show language names instead of code

--- frontend/src/components/modals/settings.jsx
+++ frontend/src/components/modals/settings.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import ISO6391 from 'iso-639-1';
+import localeEmoji from 'locale-emoji';
 import Modal from '../common/modal';
 import Select from '../common/select';
 import theme from '../../theme';
@@ -10,15 +12,23 @@ const Settings = () => {
 
   const [themeId, setThemeId] = useState('');
 
-  useEffect(() => {
-    setThemeId(theme.getCurrentId());
-  }, []);
+  const languagesData = languages.sort().map(lang => {
+    const isoCode = lang.slice(0, 2);
+    return {
+      id: lang,
+      name: `${localeEmoji(lang)} ${ISO6391.getNativeName(isoCode)}`
+    };
+  });
 
   const handleThemeChange = ({ target }) => {
     const newThemeId = target.value;
     theme.change(newThemeId);
     setThemeId(newThemeId);
   };
+
+  useEffect(() => {
+    setThemeId(theme.getCurrentId());
+  }, []);
 
   return (
     <Modal id="settings" title={t('settings')}>
@@ -33,7 +43,7 @@ const Settings = () => {
       <Select
         id="language-select"
         label={t('language')}
-        data={languages}
+        data={languagesData}
         row
         value={i18n.language}
         onChange={e => i18n.changeLanguage(e.target.value)}
